39369be4b536e0724b527fbacaa1266bbc6d4122
Zarin <zszarin021@gmail.com>
2024-12-30 21:00:01 -0700

    more

---
+++ 1768. Merge Strings Alternatively/1768_MergeStrAlt.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged_str = ''
+        i = 0
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                merged_str += word1[i]
+            if i < len(word2):
+                merged_str += word2[i]
+        return merged_str
